a7f623debdb0894810535cfa15bb7255ae2efc9a
Andrew Chan <chandrew@utexas.edu>
2014-03-01 16:24:55 -0600

    run

--- test/run-functions.php
+++ test/run-functions.php
@@ -7,9 +7,9 @@
 	// text works, but is annoying.
 	//text('+17133677805', 'Hello!');
 
-	echo '<br/>' + pg_connection_string() + '<br/>';
+	echo '<br/>Connection string: ' + pg_connection_string() + '<br/>';
 
-	echo time() + '<br/>';
+	echo 'time: ' + date("Y-m-d H:i:s",time()) + '<br/>';
 
 	/*$reminder = reminder('text', '7133677805', 'Yo yo', );
 	if ($reminder) echo $reminder;
