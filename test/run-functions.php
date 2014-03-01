594199a1b283320b54bea74c16c6df0cd3d31ad3
Andrew Chan <chandrew@utexas.edu>
2014-03-01 16:52:54 -0600

    fixing query

--- test/run-functions.php
+++ test/run-functions.php
@@ -11,10 +11,16 @@
 	$date = date('Y-m-d H:i:s');
 	echo "Current time: $date<br/>";
 
-	$reminder = reminder('text', '7133677805', 'Yo yo', $date);
+	/*$reminder = reminder('text', '7133677805', 'Yo yo', $date);
 	if ($reminder) echo "$reminder<br/>";
-	else echo 'Reminder not working';
+	else echo 'Reminder not working';*/
+
 	$select = select();
-	if ($reminder) echo "$select<br/>";
+	if ($reminder) {
+		while ($row = pg_fetch_row($select)) {
+			echo "Author: $row[0]  E-mail: $row[1]";
+			echo "$row<br />\n";
+		}
+	}
 	else echo 'Select not working';
 ?>
