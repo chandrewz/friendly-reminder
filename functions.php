0ccb0415594316a8eaa52026f4a522c3a4f02844
Andrew Chan <chandrew@utexas.edu>
2014-03-01 13:33:05 -0600

    will it fix?

--- functions.php
+++ functions.php
@@ -2,41 +2,26 @@
 	// Include the Twilio PHP library
 	require 'Services/Twilio.php';
 
-	class Functions {
+	// Set account sid and auth token
+	$sid = 'AC609c3f7d32dfb4dddf8d3b79eac93f41';
+	$token = 'f5fe765f35130f11b365a26059a9eed3';
 
-		// account sid and auth token
-		var $sid;
-		var $token;
+	// my twilio phone number
+	$phoneNumber = '832-460-0899';
 
-		// my twilio phone number
-		var $phoneNumber;
-
-		// Twilio Rest Client
-		var $client;
-
-		public function Functions() {
-			// Set account sid and auth token
-			$sid = 'AC609c3f7d32dfb4dddf8d3b79eac93f41';
-			$token = 'f5fe765f35130f11b365a26059a9eed3';
-
-			// my twilio phone number
-			$phoneNumber = '832-460-0899';
-
-			// Instantiate a new Twilio Rest Client
-			$client = new Services_Twilio($sid, $token);
-		}
-
-		public function text($number, $message) {
-			$sms = $client->account->messages->sendMessage(
-				$phoneNumber,
-				// the number we are sending to
-				$number,
-				// the sms body
-				$message
-			);
-			// Display a confirmation message on the screen
-			echo "Sent message to $number";
-		}
-
+	// Instantiate a new Twilio Rest Client
+	$client = new Services_Twilio($sid, $token);
+echo "we are in functions\n";
+	function text($number, $message) {
+		echo "we are in text\n";
+		$sms = $client->account->messages->sendMessage(
+			$phoneNumber,
+			// the number we are sending to
+			$number,
+			// the sms body
+			$message
+		);
+		// Display a confirmation message on the screen
+		echo "Sent message to $number\n";
 	}
 ?> 
--- test/run-functions.php
+++ test/run-functions.php
@@ -1,5 +1,4 @@
 <?php
 	require '../functions.php';
-	$func = new Functions();
-	$func->text('+17133677805', 'Hello!');
+	text('+17133677805', 'Hello!');
 ?>
