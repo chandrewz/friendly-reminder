d90911ed6d5fe1ed127cde98ddf6cc15ae01875d
Andrew Chan <chandrew@utexas.edu>
2014-03-02 08:57:18 -0600

    db

--- src/Database.php
+++ src/Database.php
@@ -32,9 +32,9 @@ class Database {
 	 * init: CREATE TABLE reminder(reminder_id serial, reminder_type text, reminder_to text, reminder_message text, reminder_time timestamp, reminder_sent boolean);
 	 */
 	public function addReminder($type, $to, $message, $timestamp) {
-		$query = sprintf('INSERT INTO reminder(reminder_type, reminder_to, reminder_message, reminder_time, reminder_sent) VALUES ("%s", "%s", "%s", "%s", false);',
-			$type, $to, $message, $timestamp);
-		//$query = "INSERT INTO reminder(reminder_type, reminder_to, reminder_message, reminder_time, reminder_sent) VALUES ('$type', '$to', '$message', '$timestamp', false);";
+		//$query = sprintf('INSERT INTO reminder(reminder_type, reminder_to, reminder_message, reminder_time, reminder_sent) VALUES ("%s", "%s", "%s", "%s", false);',
+		//	$type, $to, $message, $timestamp);
+		$query = "INSERT INTO reminder(reminder_type, reminder_to, reminder_message, reminder_time, reminder_sent) VALUES ('$type', '$to', '$message', '$timestamp', false);";
 		return pg_query($this->db, $query);
 	}
 
@@ -45,8 +45,8 @@ class Database {
 	public function getReadyReminders() {
 		date_default_timezone_set('America/Chicago'); // using CST, because I'm in Austin!
 		$date = date('Y-m-d H:i:s');
-		$query = sprintf('SELECT * FROM reminder WHERE reminder_time <= "%s" AND reminder_sent = false;', $date);
-		//$query = "SELECT * FROM reminder WHERE reminder_time <= CURRENT_TIMESTAMP AND reminder_sent = false;";
+		//$query = sprintf('SELECT * FROM reminder WHERE reminder_time <= "%s" AND reminder_sent = false;', $date);
+		$query = "SELECT * FROM reminder WHERE reminder_time <= '$date' AND reminder_sent = false;";
 		return pg_query($this->db, $query);
 	}
 
