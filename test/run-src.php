e1ae4784f60ed86f0e337aeaf83ffc388e530fec
Andrew Chan <chandrew@utexas.edu>
2014-03-01 20:09:18 -0600

    db

--- test/run-src.php
+++ test/run-src.php
@@ -1,5 +1,15 @@
 <?php
 	require '../src/Contactor.php';
 
+	echo 'Begin text test.';
 	$contactor = new Contactor();
 	$contactor->text('7133677805', 'Please work!');
+	echo 'Text sent<br/>';
+
+	$db = new Database();
+	echo $db->pgConnectionString();
+	$connected = $db->connect();
+	if ($connected) {
+		echo 'Connected!<br/>';
+	}
+	else echo 'Not connected :(<br/>';
