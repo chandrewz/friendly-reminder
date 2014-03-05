d9ba129bc5b03e797eca8d764a584604ff5a4a28
Andrew Chan <chandrew@utexas.edu>
2014-03-05 15:30:51 -0600

    friend api

---
+++ friend-api.php
@@ -0,0 +1,9 @@
+<?php
+	if (isset($_GET['user'])) {
+		require_once('src/Database.php');
+		$db = new Database();
+		$db->connect();
+		$friends = $db->getFriends($_GET['user']);
+		echo json_encode(pg_fetch_all($friends));
+	}
+?>
