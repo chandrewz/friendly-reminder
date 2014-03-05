aa597b9204064d7dc93e2cd1655d8987da360ba9
Andrew Chan <chandrew@utexas.edu>
2014-03-05 15:15:51 -0600

    missing _

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 		require_once('src/Database.php');
 		$db = new Database();
 		$db->connect();
-		$db->addReminder($_POST['option'], $_POST['number'], $_POST['message'], $POST['time']);
-		echo 'Message sent!';
+		$db->addReminder($_POST['option'], $_POST['number'], $_POST['message'], $_POST['time']);
+		echo "Your $_POST['option'] to $_POST['number'] has been scheduled on $_POST['time']!";
 	}
 ?>
 
