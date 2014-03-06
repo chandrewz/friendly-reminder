6cdf5fc1c0aeb555ce3806f3b6e066ae74b63dc3
Andrew Chan <chandrew@utexas.edu>
2014-03-05 18:33:52 -0600

    missing underscore!!!

--- test/test-runner.php
+++ test/test-runner.php
@@ -8,10 +8,10 @@
 	//$contactor->text('7133677805', 'Please work!');
 	//echo "Text sent\n<br/>";
 	
-	$contactor->call('7133677805', 'HELLO%20HELLO%20HELLO%20DOGE%20SHIBE');
+	//$contactor->call('7133677805', 'HELLO%20HELLO%20HELLO%20DOGE%20SHIBE');
 	
 
-	/*$db = new Database();
+	$db = new Database();
 	echo $db->pgConnectionString();
 	$connected = $db->connect();
 	if ($connected) {
@@ -20,16 +20,21 @@
 		$date = date('Y-m-d H:i:s');
 		echo "Current time: $date\n<br/>";
 
-		$added = $db->addReminder('text', '8652296478', 'addReminder message', $date);
+		//$added = $db->addReminder('text', '8652296478', 'addReminder message', $date);
 		if ($added) echo "Reminder added\n<br/>";
 		else echo "addReminder failed\n<br/>";
 
-		$ready = $db->getReadyReminders();
+		//$ready = $db->getReadyReminders();
 		if ($ready) {
 			echo print_r(pg_fetch_all($ready));
 		} else echo "getReadyReminders() failed\n<br/>";
 
-		$db->addFriend("chandrew", "jlin");
+		//$db->addFriend("chandrew", "jlin");
 		echo print_r(pg_fetch_all($db->getFriends("chandrew")));
+
+		echo "Get user\n";
+		$user = $db->getUser("chandrew");
+		$rows = pg_num_rows($user);
+		echo $rows . " row(s) returned.\n";
 	}
-	else echo "Not connected :(\n<br/>";*/
+	else echo "Not connected :(\n<br/>";
