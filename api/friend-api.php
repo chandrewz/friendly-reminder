5e421e88951736cdc2b3911fbb2807f7d8f454e0
Andrew Chan <chandrew@utexas.edu>
2014-03-05 22:02:34 -0600

    api location change

--- api/friend-api.php
+++ api/friend-api.php
@@ -1,10 +1,11 @@
 <?php
-	header("Content-Type:text/plain");
-	if (isset($_GET['user'])) {
-		require_once('src/Database.php');
-		$db = new Database();
-		$db->connect();
-		$friends = $db->getFriends($_GET['user']);
-		echo json_encode(pg_fetch_all($friends));
-	}
-?>
+
+header("Content-Type:text/plain");
+
+if (isset($_GET['user'])) {
+	require_once('../src/Database.php');
+	$db = new Database();
+	$db->connect();
+	$friends = $db->getFriends($_GET['user']);
+	echo json_encode(pg_fetch_all($friends));
+}
